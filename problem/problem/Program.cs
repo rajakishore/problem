58c84fa0de7724f5a577f8d37f211c9fb649756f
Raja Kishore <raja.rajakishore@gmail.com>
2018-11-08 14:11:58 +0800

    Added files by upload

---
+++ problem/problem/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> result = new List<string>();
+
+            RecipientJson r = new RecipientJson();
+            var recipientList = r.GetJson();
+            List<Recipient> recipients = recipientList.recipients.ToList();
+            
+            for (int i = 0; i < recipients.Count ; i++)
+            {
+                for (int j = i+1; j < recipients.Count; j++)
+                {
+                    var matches = recipients[i].tags.Intersect(recipients[j].tags).ToList();
+                    if(matches.Count >= 2)
+                    {
+                      string matchFound = recipients[i].name + "," + recipients[j].name;
+                        result.Add(matchFound);
+                      Console.Write(matchFound + "|");
+                    }
+                }
+
+            }
+            Console.Read();
+
+        }
+    }
+}
---
+++ problem/problem/Recipient.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem
+{
+    public class Recipient
+    {
+        public List<object> tags { get; set; }
+        public string name { get; set; }
+        public int id { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Recipient> recipients { get; set; }
+    }
+
+    public class RecipientJson
+    {
+        public RootObject GetJson()
+        {
+            string json = null;
+
+            using (StreamReader r = new StreamReader(@"../../Content/data.json"))
+            {
+                 json = r.ReadToEnd();
+            }
+            var rootObject = JsonConvert.DeserializeObject<RootObject>(json);
+
+            return rootObject;
+        }
+    }
+   
+}
